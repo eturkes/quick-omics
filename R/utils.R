2346c7932a34e32e76cb2e18a097f86d2899314a
Emir Turkes <eturkes@bu.edu>
2020-06-23 07:19:10 -0400

    Add institution to copyright

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-# Copyright 2020 Emir Turkes
+# Copyright 2020 Emir Turkes, UK DRI at UCL
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
